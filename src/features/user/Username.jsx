0f86fecd71c4169b678fec3f119983b6b3e06535
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-21 05:49:32 +0300

    Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold hidden">Momen</div>;
+  return <div className="text-sm font-semibold hidden md:block">Momen</div>;
 }
 
 export default Username;
