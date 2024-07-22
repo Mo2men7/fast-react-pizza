94a4f8c13e63ce72900e3b857b4b124c313b3f31
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-22 21:14:09 +0300

    Reading and Updating the User State

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-sm font-semibold hidden md:block">Momen</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
+  );
 }
 
 export default Username;
