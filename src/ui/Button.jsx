3c076e17af8f8e4af1f51a39dc50a59d88c8bc31
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-22 03:41:35 +0300

    Reusing Styles With React Components

---
+++ src/ui/Button.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to }) {
+  const className =
+    "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring sm:px-6 sm:py-4 focus:ring-yellow-400 focus:bg-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed";
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
