f1e3ff8035354256d3f6cbcf7d547a702462f9e8
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-22 06:21:40 +0300

    Styling the Menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring sm:px-6 sm:py-4 focus:ring-yellow-400 focus:bg-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:bg-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed";
+  const styles = {
+    primary: base + " py-3 px-4 md:px-6 md:py-4",
+    small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
+  };
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
