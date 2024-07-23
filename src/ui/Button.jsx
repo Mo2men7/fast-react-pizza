f0f5fc76a2de3d8dbff2819f4706d47c45c6405b
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-23 19:44:41 +0300

    Updating Cart Quantities

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,7 @@ function Button({ children, disabled, to, type, onClick }) {
     small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
     secondary:
       "text-sm border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm ",
   };
   if (to)
     return (
